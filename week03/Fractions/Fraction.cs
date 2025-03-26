9d340af1d9456e7fe08eebedc23f538715091a27
landonjsearle <landonjsearle@gmail.com>
2025-03-25 19:18:08 -0600

    Finished Fraction class.

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -5,19 +5,50 @@ public class Fraction
     private int _top;
     private int _bottom;
 
-    Fraction()
+    public Fraction()
     {
         _top = 1;
         _bottom = 1;
     }
-    Fraction(int wholeNubmer)
+    public Fraction(int wholeNubmer)
     {
         _top = wholeNubmer;
         _bottom = 1;
     }
-    Fraction(int top, int bottom)
+    public Fraction(int top, int bottom)
     {
         _top = top;
         _bottom = bottom;
+    }
+
+    public int getNumerator()
+    {
+        return _top;
+    }
+
+    public void setNumerator(int numerator)
+    {
+        _top = numerator;
+    }
+
+
+    public int getDenominator()
+    {
+        return _bottom;
+    }
+
+    public void setDenominator(int denominator)
+    {
+        _bottom = denominator;
+    }
+
+    public String getFractionString()
+    {
+        return $"{_top} / {_bottom}";
+    }
+
+    public double getFractionDecimal()
+    {
+        return (double)_top / (double)_bottom;
     }
 }
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -6,7 +6,20 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
 
-        Fraction fraction = new Fraction();
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.getFractionString());
+        Console.WriteLine(f1.getFractionDecimal());
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.getFractionString());
+        Console.WriteLine(f2.getFractionDecimal());
+        Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine(f3.getFractionString());
+        Console.WriteLine(f3.getFractionDecimal());
+
+        f1.setNumerator(10);
+        Console.WriteLine(f1.getNumerator());
+        f2.setDenominator(10);
+        Console.WriteLine(f2.getDenominator());
 
     }
 }
